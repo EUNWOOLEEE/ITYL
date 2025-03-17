4d8db03e27746ab6c58c2f069950611afe059c42
eunwoo <lkjin0110@gmail.com>
2025-03-17 20:56:37 +0900

    init

---
+++ frontend/yes24/js/main.js
@@ -0,0 +1,12 @@
+// const $tabMember = document.querySelector('.member');
+// const $tabGuest = document.querySelector('.guest');
+
+// function clickLoginTab(clickedTab, anotherTab) {
+// 	console.log(clickedTab);
+// 	console.log(anotherTab);
+// 	clickedTab.classList.add('clicked');
+// 	anotherTab.classList.add('unclicked');
+// }
+
+// $tabMember.addEventListener('click', clickLoginTab($tabMember, $tabGuest));
+// $tabGuest.addEventListener('click', clickLoginTab($tabGuest, $tabMember));
