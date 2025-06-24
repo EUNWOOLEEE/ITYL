983f348434859a2dfa7c243295e9c477775f09dc
eunwoo <lkjin0110@gmail.com>
2025-06-24 13:52:19 +0900

    part15_basic

---
+++ backend/practice/server/part15/secserver/src/main/java/org/scoula/controller/SecurityController.java
@@ -0,0 +1,47 @@
+package org.scoula.controller;
+
+import java.security.Principal;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Log4j2
+@RequestMapping("/security")
+@Controller
+public class SecurityController {
+
+    @GetMapping("/logout")
+    public void logout() {
+        log.info("logout page");
+    }
+
+    @GetMapping("/login")
+    public void login() {
+        log.info("login page");
+    }
+
+    @GetMapping("/all") //모두 접근 가능
+    public void doAll() { //void인 경우 /security/all.jsp를 호출
+        log.info("do all can access everybody");
+    }
+
+//    @GetMapping("/member") //member권한 가진 사람, admin권한 가진 사람 접근 가능
+//    public void doMember() {
+//        log.info("logined member");
+//    }
+
+    @GetMapping("/member")
+    public void doMember(Principal principal) {
+        log.info("login member details info >> " + principal);
+        log.info("login member details info >> " + principal.getName());
+    }
+
+    @GetMapping("/admin") //admin권한 가진 사람 접근 가능
+    public void doAdmin() {
+        log.info("admin only");
+    }
+
+}
